b555bf70945980d4f50be8a6f478d8755cc4dd46
yourname <your@email.com>
2020-08-09 23:55:32 +0800

    [WangZuowen]feat:Pass unit testing.

--- src/test/java/com/thoughtworks/rslist/service/RsServiceTest.java
+++ src/test/java/com/thoughtworks/rslist/service/RsServiceTest.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.rslist.service;
 
+import com.thoughtworks.rslist.domain.Trade;
 import com.thoughtworks.rslist.domain.Vote;
 import com.thoughtworks.rslist.dto.RsEventDto;
+import com.thoughtworks.rslist.dto.TradeDto;
 import com.thoughtworks.rslist.dto.UserDto;
 import com.thoughtworks.rslist.dto.VoteDto;
 import com.thoughtworks.rslist.repository.RsEventRepository;
@@ -11,15 +13,20 @@ import com.thoughtworks.rslist.repository.VoteRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.http.MediaType;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class RsServiceTest {
   RsService rsService;
@@ -91,5 +98,88 @@ class RsServiceTest {
         () -> {
           rsService.vote(vote, 1);
         });
+  }
+  @Test
+  void shouldBuyRsEventWhenNoCompetition() throws Exception {
+    UserDto userDto =
+            UserDto.builder()
+                    .voteNum(10)
+                    .phone("188888888888")
+                    .gender("female")
+                    .email("a@b.com")
+                    .age(19)
+                    .userName("idolice")
+                    .build();
+    RsEventDto rsEventDto =
+            RsEventDto.builder().keyword("无分类").eventName("第一条事件").id(1).rank(1).user(userDto).build();
+    Trade trade = new Trade(400,1);
+    TradeDto tradeDto = TradeDto.builder().rsEventDto(rsEventDto).amount(400).rank(1).build();
+    when(rsEventRepository.findById(1)).thenReturn(Optional.of(rsEventDto));
+    when(tradeRepository.findByRank(trade.getRank())).thenReturn(null);
+    rsService.buy(trade,1);
+    verify(tradeRepository).save(tradeDto);
+    verify(rsEventRepository).save(rsEventDto);
+  }
+  @Test
+  void shouldBuyRsEventWhenAmountBetter() throws Exception {
+    UserDto userDto =
+            UserDto.builder()
+                    .voteNum(10)
+                    .phone("188888888888")
+                    .gender("female")
+                    .email("a@b.com")
+                    .age(19)
+                    .userName("idolice")
+                    .build();
+    RsEventDto rsEventDto =
+            RsEventDto.builder().keyword("无分类").eventName("第一条事件").rank(1).id(1).user(userDto).build();
+    RsEventDto rsEventDtoNew =
+            RsEventDto.builder().keyword("无分类").eventName("第一条事件").rank(1).id(2).user(userDto).build();
+    Trade trade = new Trade(400,1);
+    TradeDto tradeDto = TradeDto.builder().rsEventDto(rsEventDto).amount(200).rank(1).build();
+    TradeDto tradeDtoNew = TradeDto.builder().rsEventDto(rsEventDtoNew).amount(400).rank(1).build();
+    when(rsEventRepository.findById(2)).thenReturn(Optional.of(rsEventDtoNew));
+    when(tradeRepository.findByRank(trade.getRank())).thenReturn(tradeDto);
+    rsService.buy(trade,2);
+    verify(tradeRepository).save(tradeDtoNew);
+    verify(rsEventRepository).save(rsEventDtoNew);
+    verify(tradeRepository).deleteById(tradeDto.getId());
+    verify(rsEventRepository).deleteById(rsEventDto.getId());
+  }
+  @Test
+  void shouldReturn400WhenAmountFewer() throws Exception {
+    UserDto userDto =
+            UserDto.builder()
+                    .voteNum(10)
+                    .phone("188888888888")
+                    .gender("female")
+                    .email("a@b.com")
+                    .age(19)
+                    .userName("idolice")
+                    .build();
+    RsEventDto rsEventDto =
+            RsEventDto.builder().keyword("无分类").eventName("第一条事件").rank(1).id(1).user(userDto).build();
+    RsEventDto rsEventDtoNew =
+            RsEventDto.builder().keyword("无分类").eventName("第一条事件").rank(1).id(2).user(userDto).build();
+    Trade trade = new Trade(400,1);
+    TradeDto tradeDto = TradeDto.builder().rsEventDto(rsEventDto).amount(600).rank(1).build();
+    TradeDto tradeDtoNew = TradeDto.builder().rsEventDto(rsEventDtoNew).amount(400).rank(1).build();
+    when(rsEventRepository.findById(2)).thenReturn(Optional.of(rsEventDtoNew));
+    when(tradeRepository.findByRank(trade.getRank())).thenReturn(tradeDto);
+    assertThrows(
+            Exception.class,
+            () -> {
+              rsService.buy(trade,2);
+            });
+  }
+  @Test
+  void shouldReturn400WhenRsEventIdWrong() throws Exception {
+    Trade trade = new Trade(400,1);
+    when(rsEventRepository.findById(2)).thenReturn(Optional.empty());
+    assertThrows(
+            Exception.class,
+            () -> {
+              rsService.buy(trade,2);
+            });
   }
 }
